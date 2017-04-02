1da7a400d39d41f68933dc719b669fcf4ec6e1fb
Cristian Toaquiza <crissmtl@gmail.com>
2017-04-01 19:46:11 -0500

    POST con express

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/hola/:name', (req, res) => {
-  res.send({ message: `Hola ${req.params.name}!`});
+app.get('/api/product', (req, res) => {
+  res.status(200).send({ products: [] });
 });
+
+app.get('/api/product/:productId', (req, res) => {});
+
+app.post('/api/product', (req, res) => {
+  console.log(req.body);
+  res.status(404).send({ message: 'El producto no existe' })
+});
+
+app.put('/api/product/:productId', (req, res) => {});
+
+app.delete('/api/product/:productId', (req, res) => {});
 
 app.listen(port, () => {
   console.log(`API REST corriendo en http://localhost:${port}`);
